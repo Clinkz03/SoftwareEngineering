83c8698b2b2f7173e9354f07e95407225dcd6005
Clinkz03 <heyserjansalis@gmail.com>
2025-03-03 14:32:23 +0000

    allowed users changes

--- clodes/views.py
+++ clodes/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import MyClothes
 
 # Create your views here.
-def home(response):
-    return render(response, template_name='index.html')
+def home(request):
+    return render(request, template_name='index.html')
 def clothing(request):
     clothing=MyClothes.objects.all()
     context={'clothing':clothing}
     return render(request, template_name='clothing.html')
-def contact(response):
-    return render(response, template_name='contact.html')
-def checkout(response):
-    return render(response, template_name='checkout.html')
+def contact(request):
+    return render(request, template_name='contact.html')
+def checkout(request):
+    return render(request, template_name='checkout.html')
