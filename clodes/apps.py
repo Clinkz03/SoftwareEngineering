b0b5114ea8a5678fafd63db6d60c5866adcea7da
Heyser <heyserjansalis@gmail.com>
2025-03-05 20:31:43 +0000

    Delete clodes directory

--- clodes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClodesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clodes'
--- clodes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.home ),
-    path('clothing/', views.clothing),
-    path('checkout/', views.checkout),
-    path('contact/', views.contact),
-]
