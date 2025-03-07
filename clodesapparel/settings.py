2506656ff999035f31607965baaf3513ef59a330
Clinkz03 <heyserjansalis@gmail.com>
2025-03-07 15:36:08 +0000

    whitenoise

--- clodesapparel/settings.py
+++ clodesapparel/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clodes',
     'cart',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'clodesapparel.urls'
